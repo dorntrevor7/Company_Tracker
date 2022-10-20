b72b83b39f2b85e63cfbc5ce04fa4c3da8de66ab
dorntrevor7 <dorntrevor7@gmail.com>
2022-10-19 22:26:33 -0700

    switch for opener

--- index.js
+++ index.js
@@ -51,8 +51,49 @@ function opener() {
       },
     ])
     .then((data) => {
-      console.table(data);
-
+      console.table(data.openerQuestion);
+      switch (data.openerQuestion) {
+        case "Update Employee Row":
+          function updateEmployeeRow() {
+            inquirer
+              .prompt([
+                {
+                  type: "list",
+                  message: "What would you like to do?",
+                  name: "openerQuestion",
+                  choices: [
+                    "Update Employee Row",
+                    "View All Roles",
+                    "Add Role",
+                    "View All Departments",
+                    "Add Departments",
+                    "Quit",
+                    "View All Employees",
+                  ],
+                },
+              ])
+              .then((data) => {
+                console.table(data.openerQuestion);
+              });
+          }
+          console.log("function updateEmployeeRow");
+          break;
+        case "View All Roles":
+          console.log("function viewAllRoles");
+          break;
+        case "View All Departments":
+          console.log("function addRole");
+          break;
+        case "Add Departments":
+          console.log("function addDepartments");
+          break;
+        case "Quit":
+          console.log("function quit");
+          break;
+        case "View All Employees":
+          console.log("function viewAllEmployees");
+          break;
+      }
       // const filename = `${data.name.toLowerCase().split(" ").join("")}.json`;
 
       // fs.writeFile(filename, JSON.stringify(data, null, "\t"), (err) =>
